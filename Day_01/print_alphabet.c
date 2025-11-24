59e86f3549986c71c8093388963bbd1bcae29a8e
GHIZLANE <Ghizlaneelouakili7@gmail.com>
2025-11-24 16:58:43 +0100

    Day 1: hello , character , print_alphabet

---
+++ Day_01/print_alphabet.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void print_alphabet(void)
+{
+    char c;
+    
+    c = 'a';
+    while (c <= 'z')
+    {
+        write(1, &c, 1);
+        c++;
+    }
+}
+
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
